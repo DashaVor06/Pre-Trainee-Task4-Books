9736cabc6c9a5ad8809ddc3fabb722a1c3e60afc
DashaVor06 <dashasolnyshko.ds@gmail.com>
2025-10-21 13:04:54 +0300

    Add project files.

---
+++ Models/Authors.cs
@@ -0,0 +1,10 @@
+﻿namespace Books.Models
+{
+    public class Authors
+    {
+        public string Author { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
